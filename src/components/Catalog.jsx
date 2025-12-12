285ab97a907ceb08e736375d97b6b06f2206f390
Navin Kunakornvanich <nkunakornvanich@zagmail.gonzaga.edu>
2025-12-11 17:32:48 -0800

    setup frontend

---
+++ src/components/Catalog.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+import './catalog.css'
+
+export default function Catalog(){
+  return (
+    <div className="catalog">
+        <h2>Catalog Component</h2>
+    </div>
+  )
+}
